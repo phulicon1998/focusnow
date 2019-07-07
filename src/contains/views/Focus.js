a7258475c33a460b9ce737cbdbfb90f23fb7f445
phulicon1998 <phulicon815490@gmail.com>
2019-07-07 23:10:53 +0700

    Done adapting the figma design into the app

--- src/contains/views/Focus.js
+++ src/contains/views/Focus.js
@@ -5,50 +5,77 @@ const electron = window.require("electron");
 const ipc = electron.ipcRenderer;
 
 export default function FocusContain(props) {
-    const [state, setState] = useState({
+    const [progress, setProgress] = useState({
         total: 10,
         left: 10,
-        round: 1,
-        cont: true
+        stage: 5,
+        cont: true,
+        breakTime: false
     });
-    const {total, left, cont} = state;
+    const [time] = useState({
+        work: 10,
+        short: 5,
+        long: 10,
+        round: 5
+    })
+    let {left, cont, breakTime, stage} = progress;
     let interval;
 
-    // useEffect(() => {
-    //     decrease();
-    //     return () => clearInterval(interval);
-    // })
+    useEffect(() => {
+        decrease(reset);
+        return () => clearInterval(interval);
+    })
+
+    function getProgressType() {
+        if(breakTime) return time.work;
+        return stage - 1 > 0 ? time.short : time.long;
+    }
 
     function decrease(cb) {
         if(cont) {
             interval = setInterval(() => {
                 if(left === 0){
-                    clearInterval(interval);
-                    return cb ? cb() : null;
+                    if(stage === 0) {
+                        clearInterval(interval);
+                        return cb ? cb() : null;
+                    }
+                    setProgress(prev => ({
+                        ...prev,
+                        breakTime: !prev.breakTime,
+                        total: getProgressType(),
+                        left: getProgressType(),
+                        stage: prev.breakTime ? prev.stage : prev.stage - 1
+                    }))
                 }
-                setState(prev => ({...prev, left: prev.left - 1 }));
+                if(left !== 0) setProgress(prev => ({...prev, left: prev.left - 1 }));
             }, 1000);
         }
     }
 
     function reset() {
         clearInterval(interval);
-        setState({...state, left: total, cont: false});
+        setProgress({
+            ...progress,
+            left: time.work,
+            cont: false,
+            breakTime: false,
+            stage: time.round
+        });
     }
 
     function pause() {
         if(cont) {
             clearInterval(interval);
-            return setState({...state, cont: !cont});
+            return setProgress({...progress, cont: !cont});
         }
-        setState({...state, cont: !cont});
+        setProgress({...progress, cont: !cont});
         return decrease();
     }
 
     const cancel = () => ipc.send("restore-main");
 
     return <Focus
-        {...state}
+        {...progress}
         {...props}
         reset={reset}
         pause={pause}
--- src/presents/views/Focus.js
+++ src/presents/views/Focus.js
@@ -6,13 +6,19 @@ import {ReactComponent as Fpause} from "../../assets/icon/f_pause.svg";
 import {ReactComponent as Freset} from "../../assets/icon/f_reset.svg";
 import {ReactComponent as Fcancel} from "../../assets/icon/f_cancel.svg";
 
-
-const Focus = ({total, left, round, reset, pause, cancel}) => (
+const Focus = ({total, left, stage, reset, pause, cancel, cont, breakTime}) => (
     <div className="container">
-        <span className="round">5</span>
-        <span className="progress-bar"></span>
+        <span className="round">{breakTime ? "b" : stage}</span>
+        <div className="progress-box">
+            <div style={{
+                width: `${(left/total)*159}px`,
+                backgroundColor: `${breakTime ? "#CEA791" : "#8AE9D7"}`
+            }}></div>
+        </div>
         <div className="control">
-            <button onClick={pause} className="play"><Fplay /></button>
+            <button onClick={pause} className={cont ? "pause" : "play"}>
+                {cont ? <Fpause /> : <Fplay />}
+            </button>
             <button onClick={reset}><Freset /></button>
         </div>
         <button className="cancel" onClick={cancel}><Fcancel /></button>
