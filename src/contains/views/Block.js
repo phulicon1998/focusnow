d69955e7c22f64faa7c215782be2544b4f7c8c09
SOI-LAN-TRON <truongquanghieu8198@gmail.com>
2019-07-09 23:51:22 +0700

    Not complete block website

--- src/contains/views/Block.js
+++ src/contains/views/Block.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import Block from "../../presents/views/Block";
 
 const electron = window.require('electron');
 const ipc = electron.ipcRenderer;
 
-export default function BlockContain(props) {
-    // const [list, setList] = useState([]);
+export default function BlockContain(props) {  
+    // // const [list, setList] = useState([]);
+    const [state, setState] = useState({
+        link: '',
+        id: '',
+        list: [],
+        actived: true
+    });
+    const {link, id, actived} = state;
+
+    const hdChange = (e) => {
+        setState({
+            [e.targe.name]: e.target.value
+        })
+    }
+
+    function hdSubmit(e) {
+        e.preventDefault();
+        let{link, id, actived, list} = state;
+        if(id === ""){
+            list = [...list, {link, actived}];
+        }else {
+            list[id] = {...list[id], link, actived: false};
+        }
+        setState({...state, list, link: "", id: "", actived: true});
+    }
+
+    function hdActive(id) {
+        let {list} = state;
+        setState({...state, ...list[id], id});
+    }
+
+    function hdRemove(i) {
+        let {list} = state;
+        list.splice(i, 1);
+        setState({...state, list});
+    }
 
     const block = () => ipc.send("begin-block");
     const unblock = () => ipc.send("stop-block");
 
-    return <Block {...props} block={block} unblock={unblock}/>
+    return <Block
+        {...props}
+        {...state}
+        hdChange = {hdChange}
+        hdSubmit = {hdSubmit}
+        hdRemove = {hdRemove}
+        hdActive = {hdActive}
+        block={block}
+        unblock={unblock}
+    />
 }
